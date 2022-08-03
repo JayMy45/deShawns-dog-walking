27ca1131b53d303f4fa5830c80973fca2b38177a
Jeremy Myers <echelonical@gmail.com>
2022-08-03 09:56:58 -0500

    'click' eventListner added to walkerName

--- scripts/database.js
+++ scripts/database.js
@@ -101,10 +101,10 @@ const database = {
 }
 
 export const getWalkers = () => {
-    return database.walkers.map(walker => ({...walker}))
+    return database.walkers.map(walker => ({ ...walker }))
 }
 
 export const getPets = () => {
-    return database.pets.map(pet => ({...pet}))
+    return database.pets.map(pet => ({ ...pet }))
 }
 
